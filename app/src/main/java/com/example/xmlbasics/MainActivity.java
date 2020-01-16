9193e6ebce1ce086f18079f62de673fe2fe302a1
Michael Highsmith <Phonlabtech@gmail.com>
2020-01-15 21:07:17 -0600

    ConnectingLayoutWithAClass

--- app/src/main/java/com/example/xmlbasics/MainActivity.java
+++ app/src/main/java/com/example/xmlbasics/MainActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //When the app launches, do everything in between the brackets
     protected void onCreate(Bundle savedInstanceState) {
+        //Do some background things the same way as usual
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //Set the apps screen to the layout activity_main
+        setContentView(R.layout.second_layout);
     }
 }
